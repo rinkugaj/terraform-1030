409ab0c9914529836c1e3cf209e757d8f9d59035
GuestRinku <rinkugajbhiye1111@gmail.com>
2025-10-31 00:34:53 +0530

    main.tf added

---
+++ Day-2-Terraform-configuration-files/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description = "passing ami value"
+    default = "ami-0bdd88bd06d16ba03"
+    type = string
+  
+}
+variable "type" {
+    description = "passing value of intance type"
+    default = "t2.micro"
+    type = string
+  
+}
